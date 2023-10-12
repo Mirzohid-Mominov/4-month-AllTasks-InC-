97af4a1920118635421db7478c818e12b139b7a8
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-10-12 10:35:04 +0500

    finished

---
+++ 4-month_AllTasks_InC#/Delegate Example/LinqExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Example
+{
+    public static class LinqExtensions
+    {
+
+        public static IEnumerable<(TSource, TSource)> ZipIntersectBy<TSource, TKey>(
+            this ICollection<TSource> sourceA,
+            ICollection<TSource> sourceB,
+            Func<TSource, TKey> keySelector
+            ) where TKey : notnull
+        {
+            if (sourceA is null)
+                throw new ArgumentNullException(nameof(sourceA));
+
+            if (sourceB is null)
+                throw new ArgumentNullException(nameof(sourceB));
+
+            if (keySelector is null)
+                throw new ArgumentNullException(nameof(keySelector));
+            return ZipIntersectByIterator(sourceA, sourceB, keySelector);
+        }
+
+        private static IEnumerable<(TSource, TSource)> ZipIntersectByIterator<TSource, TKey>(
+            this ICollection<TSource> sourceA,
+            ICollection<TSource> sourceB,
+            Func<TSource, TKey> keySelector
+            ) where TKey : notnull
+        {
+            foreach (var itemA in sourceA)
+            {
+                var key = keySelector(itemA);
+                var itemB = sourceB.FirstOrDefault(element => keySelector(element).Equals(key));
+
+                if (itemB != null)
+                    yield return (itemA, itemB);
+            }
+        }
+    }
+}
+ 
---
+++ 4-month_AllTasks_InC#/Delegate Example/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Delegate_Example;
+
+var postA = new List<BlogPostA>
+{
+    new(Guid.Parse("5ED80943-8950-48B2-A5EC-28F5F143C968"), "SunLight", "rising"),
+    new(Guid.Parse("5E172C91-79A9-4FD4-8BE1-78C2DA576E61"), "Moon", "shining")
+};
+
+var postB = new List<BlogPostA>
+{
+    new(Guid.Parse("5ED80943-8950-48B2-A5EC-28F5F143C968"), "About fish", "sdfg"),
+    new(Guid.Parse("5E172C91-79A9-4FD4-8BE1-78C2DA576E61"), "About Students", "sdfg")
+};
+
+var posts = postA.ZipIntersectBy(postB, post => post.Id);
+
+foreach (var (post1, post2) in posts)
+{
+    Console.WriteLine($"Eskisi - {post1.Topic} : {post1.Title}");
+    Console.WriteLine($"Yangisi - {post2.Topic} : {post2.Title}");
+}
+
+public class BlogPostA
+{
+    public Guid Id { get; set; }
+    public string Topic { get; set; }
+    public string Title { get; set; }
+
+    public BlogPostA(Guid id, string topic, string title)
+    {
+        Id = id;
+        Topic = topic;
+        Title = title;
+    }
+}
