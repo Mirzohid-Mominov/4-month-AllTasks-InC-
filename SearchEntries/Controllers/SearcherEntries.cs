b872118e3c9885b51770b574d42f5828fe950493
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-10-20 19:25:14 +0500

    Searcher files API

---
+++ SearchEntries/Controllers/SearcherEntries.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SearchEntries.Services;
+
+namespace SearchEntries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearcherEntries : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromServices] Searching searching, string fileName)
+        {
+            return Ok(searching.SearchDirectoryName(fileName));
+        }
+    }
+}
---
+++ SearchEntries/Services/Searching.cs
@@ -0,0 +1,25 @@
+﻿namespace SearchEntries.Services
+{
+    public class Searching
+    {
+        public string SearchDirectoryName(string fileName)
+        {
+            var directory = Directory.GetDirectories(@"D:\\", "*", SearchOption.TopDirectoryOnly);
+
+            for (var item = 0; item < directory.Length; item++)
+            {
+                var foundFile = SearchFileName(directory[item], fileName);
+                if (foundFile != null)
+                    return foundFile;
+
+            }
+                return "There isn't this file";
+        }
+
+        public string SearchFileName(string path, string fileName)
+        {
+            var files = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories);
+            return files.FirstOrDefault(x => Path.GetFileName(x).Equals(fileName));
+        }
+    }
+}
