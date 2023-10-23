921931075bcb9e864c66049c25394d47f525f74e
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-10-23 19:13:50 +0500

    ui lesson task

---
+++ DemoApp/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using DemoApp;
+using DemoApp.Dtos;
+using DemoApp.Entities;
+using Sharprompt;
+
+var service = new UserService();
+
+while(true)
+{
+    object choose = Prompt.Select("Tanlang : ", new[] { "User yaratish", "Get users" });
+
+    if(choose == "User yaratish")
+    {
+        var firstName = Prompt.Input<string>("Enter your FirstName ");
+        var lastName = Prompt.Input<string>("Enter your LastName ");
+        var email = Prompt.Input<string>("Enter your email ");
+        var password = Prompt.Input<string>("Enter your password ");
+        var userName = Prompt.Input<string>("Enter your userName ");
+
+        var user = new UserForCreation()
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Password = password,
+            UserName = userName
+        };
+
+        service.Create(user);
+    }
+
+    else if(choose == "Get users")
+    {
+        var users = service.GetUsers();
+
+        foreach(var user in users)
+        {
+            Console.WriteLine($"{user.FirstName} {user.LastName} {user.UserName} {user.CreatedAt}");
+            Prompt.Input<string>(" ");
+        }
+    }
+
+    Console.Clear();
+}    
---
+++ DemoApp/UserService.cs
@@ -0,0 +1,40 @@
+﻿using DemoApp.Dtos;
+using DemoApp.Entities;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp;
+
+public class UserService
+{
+    private List<User> _users = new List<User>();
+    public User Create(UserForCreation user)
+    {
+        var foundUser = _users.FirstOrDefault(x => x.Email.Equals(user.Email));
+
+        if (foundUser != null)
+        {
+            Console.WriteLine("This user already existed");
+            return foundUser;
+        }
+
+        var newUser = user.Adapt<User>();
+
+        newUser.CreatedAt = DateTime.UtcNow;
+        newUser.UpdatedAt = DateTime.UtcNow;
+        newUser.Id = Guid.NewGuid();
+
+        _users.Add(newUser);
+
+        return newUser;
+    }
+
+    public List<UserVievModel> GetUsers()
+    {
+        return _users.Adapt<List<UserVievModel>>();
+    }
+}
